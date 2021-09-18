3b13d85a81ef98d4925796ebb44050b8127d715c
Your Name <you@example.com>
2021-09-18 11:35:11 +0200

    XMLHttpRequest test

--- team_tank_tactics/app/views.py
+++ team_tank_tactics/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "app/index.html")
+
+def click(request):
+    return HttpResponse("click\n")
