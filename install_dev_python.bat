9e5c4014f19f7d1ee6231835e34bf9fef91eb23f
Stanlej42 <49549392+Stanlej42@users.noreply.github.com>
2021-09-20 11:25:57 +0200

    Create install_dev_python.bat

---
+++ install_dev_python.bat
@@ -0,0 +1,8 @@
+call python -m venv env
+cd env 
+call Scripts/activate
+call python -m pip install Django
+cd ..
+cd team_tank_tactics
+call python manage.py makemigrations 
+call python manage.py migrate
